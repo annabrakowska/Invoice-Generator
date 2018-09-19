53c71a123611c5e14819913eb4ecc355b0ac68ec
Anna <anna.brakowska91@gmail.com>
2018-09-19 10:17:32 -0400

    added prawn gem and created table

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,35 +1,37 @@
 class InvoicesController < ApplicationController
   before_action :set_invoice, only: [:show, :edit, :update, :destroy]
-
-  # GET /invoices
-  # GET /invoices.json
+  
   def index
     @invoices = Invoice.all
 
     respond_to do |format|
       format.html
+      format.pdf do 
+        pdf = InvoicePdf.new(@invoices)
+        send_data pdf.render, filename: "invoice.pdf", type: 'application/pdf'
+      end
       format.csv { send_data @invoices.to_csv, filename: "invoices-#{Date.today}.csv" }
     end
 
+
+
   end
 
 
-  # GET /invoices/1
-  # GET /invoices/1.json
+
   def show
   end
 
-  # GET /invoices/new
+  
   def new
     @invoice = Invoice.new
   end
 
-  # GET /invoices/1/edit
+  
   def edit
   end
 
-  # POST /invoices
-  # POST /invoices.json
+
   def create
     @invoice = Invoice.new(invoice_params)
 
@@ -44,8 +46,7 @@ class InvoicesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /invoices/1
-  # PATCH/PUT /invoices/1.json
+  
   def update
     respond_to do |format|
       if @invoice.update(invoice_params)
@@ -58,8 +59,7 @@ class InvoicesController < ApplicationController
     end
   end
 
-  # DELETE /invoices/1
-  # DELETE /invoices/1.json
+  
   def destroy
     @invoice.destroy
     respond_to do |format|
@@ -69,12 +69,12 @@ class InvoicesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_invoice
       @invoice = Invoice.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    
     def invoice_params
       params.require(:invoice).permit(:amount, :company, :client, :currency, :date)
     end
